94d0f4f9d3a66745d122bcf3b4f4d39a6079c8ea
Raphael <raphaelmwasaru@outlook.com>
2025-03-18 20:17:08 +0300

    Changed version number in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ficha-key",
-    version="1.0.0",
+    version="1.0.1",
     py_modules=["api", "app_config", "ficha", "logging_service", "secret_key_generator"],
     # install_requires=["requests"],
     author="Raphael Mwasaru Mwangangi",
